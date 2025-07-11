78b89de039d74408ff2ec7739f0a5c224c421d20
Sam <samuelrichardson902@gmail.com>
2025-07-11 22:38:48 +0100

    changeddd favicon

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -4,11 +4,21 @@ import { SupabaseAuthProvider } from "../components/SupabaseAuthProvider";
 export const metadata = {
   title: "Jobify",
   description: "Track your job applications with ease.",
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+    apple: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+      </head>
       <body>
         <SupabaseAuthProvider>{children}</SupabaseAuthProvider>
       </body>
